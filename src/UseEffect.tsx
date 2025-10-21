3716671f3ea3d3010c138c740cddf61d0e371fd8
cesargrivasm <cesarg.rivasm@gmail.com>
2025-10-20 22:21:21 -0400

    third commit, final revision

--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseEffect = () => {
+  const [cont, setCont]= useState(0);
+
+  useEffect(()=>{
+   setCont(cont + 1);
+   return ()=>{
+    console.log('cleanup!!');
+   }
+  },[])
+
   return (
     <div>
       <h2>Use Effect Component</h2>
+      {cont}
+
     </div>
   )
 }
